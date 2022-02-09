c1999b1064de6b5e57c40ec98eb51ed8f7a7f68a
dohyunkim <kdh951102@gmail.com>
2022-02-09 22:48:29 +0900

    Add es6 practice folder

---
+++ es6_practice/es6.js
@@ -0,0 +1,39 @@
+// // study for es6
+
+// // Variable
+
+// let hello = 'world';
+// //let hello = 'pretty';
+
+// for (i = 0; i < 10; i++) {
+//   console.log('testing');
+// }
+
+// function test() {
+//   let hello = 'korea';
+//   console.log(hello);
+// }
+
+// test();
+
+// console.log(hello);
+
+// Object
+const drinks = {}; //객체
+drinks.caffe = 'latte';
+drinks.lemon = 'eda';
+
+console.log(drinks);
+
+// Array
+const arr = [1, 2, 3, 4, 5];
+arr[0] = 100;
+arr[4] = 500;
+
+console.log(arr);
+
+// Common variable
+const var1 = 'ex_var';
+//var1 = 'anyvar';   this occur error!
+
+console.log(var1);
