dc77c5319f36a5b248c9fdc694e589a7dcb56add
dohyunkim <kdh951102@gmail.com>
2022-02-10 21:31:53 +0900

    Add React study directory

---
+++ About React/react.js
@@ -0,0 +1,9 @@
+//REACT? -> user interface 를 개발하기 위한 js library.
+
+//text, 그림, button과 같은 화면 뷰를 만들어주는 library.
+
+//State? -> Component에서 rendering되는 data를 담고 유지관리하는 js 객체. 중요!
+//State 는 class component안에서만 사용이 가능하고, 함수 component에서는 사용이 불가.
+//State는 render함수 바깥에서 정의.ㅁ
+
+//Arrow function에 대해 공부하기. 어떨 때 ()와 {}를 쓰는지도.
